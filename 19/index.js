01baa45df981c0ad90d79ef6f3fb26c2f1d26c48
wcc17 <christian.curry17@gmail.com>
2020-12-19 17:46:04 -0500

    quitting day 19 for now

--- 19/index.js
+++ 19/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const { performance } = require('perf_hooks');
 
-function getInput() {
+function getInput(filename) {
   try {
-    input = fs.readFileSync('./in.txt', 'utf8');
+    input = fs.readFileSync(filename, 'utf8');
     input = input.split("\n");
   } catch (err) {
     console.error(err);
@@ -123,15 +123,17 @@ function parseInput(input) {
 }
 
 function solve() {
-  var input = getInput();
+  let input1 = getInput('./in.txt');
+  let input2 = getInput('./in2.txt')
 
   let startTime = performance.now();
-  console.log('problem 1 solution: ' + problem1(input));
+  console.log('problem 1 solution: ' + problem1(input1));
   console.log('problem 1 execution time: ' + (performance.now() - startTime) + ' ms');
 
   startTime = performance.now();
-  console.log('problem 2 solution: ' + problem2(input));
+  console.log('problem 2 solution: ' + problem2(input2));
   console.log('problem 2 execution time: ' + (performance.now() - startTime) + ' ms');
 }
+
 
 solve();
